c942bb8fc10f1d320e5855159688f7c28af11e1a
SymAir <symeon@oclocher.fr>
2025-07-07 15:20:19 +0200

    Added Firebase Hosting

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wo_form_example/app.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: 'assets/.env');
+  await dotenv.load(fileName: 'assets/dotenv');
 
   runApp(const WoFormExamplesApp());
 }
